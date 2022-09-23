6dfe5c5091b7a54a5df623695729c6d35c04f190
Mats Larsen <me@supergrecko.com>
2022-09-23 13:07:38 +0200

    fix: adjust text and grammatical errors

--- src/home.rs
+++ src/home.rs
@@ -40,7 +40,7 @@ fn home_introduction_section() -> Html {
                     <div>
                         <Heading primary={true}>{"👋Hi, I'm Mats!"}</Heading>
                         <Paragraph>{"I'm a developer and student based in Trondheim, Norway with a passion for building things that people love. I love to explore new things, work on free open-source software, and cook food."}</Paragraph>
-                        <Paragraph>{"I'm currently studying computer science at NTNU in Trondheim, Norway, working part-time as a frontend developer remotely. I also contribute to a number of open-source projects on GitHub."}</Paragraph>
+                        <Paragraph>{"I'm currently enrolled in a bachelor's degree in informatics at NTNU in Trondheim, Norway. On the side I'm remotely working part-time as a frontend developer and working on a number of open-source projects on GitHub."}</Paragraph>
                     </div>
                 </div>
             </div>
@@ -61,19 +61,19 @@ fn home_projects_section() -> Html {
                         repo="compiler-explorer"
                         stack={vec!["typescript".to_owned(), "nodejs".to_owned()]}
                         contribution="I've worked across the entire Compiler Explorer stack, but most of my work has been on transitioning from JavaScript to TypeScript. I've also done extensive work on the code formatters and assembly documentation hints in the editor. I'm also responsible for some the Rust tooling on the site."
-                        description="Compiler Explorer, or you may know it as godbolt.org is an interactive website where you can use over 1500 compilers and inspect their assembly outputs."
+                        description="Compiler Explorer, or commonly known as godbolt.org, is an interactive website where you can use over 1500 compilers and inspect their assembly outputs in the browser."
                     />
                     <RepositoryCard
                         org="matsjla"
                         repo="league-connect"
                         stack={vec!["typescript".to_owned(), "nodejs".to_owned()]}
                         contribution="I started development on league-connect in 2019. After many iterations and a number of releases following semantic versioning it has become the most popular Node.js library for interacting with the League Client."
-                        description="League Connect is my open-source Node.js library published to NPM that enables desktop applications and tools to access the League of Legends Client APIs. It has support for connection over HTTP/1.1, HTTP/2.0 and WebSockets."
+                        description="League Connect is my first open-source Node.js library published to NPM that enables desktop applications and tools to access the League of Legends Client APIs. It works over a mix of HTTP/1.1, HTTP/2.0 and WebSockets."
                     />
                     <RepositoryCard
                         org="llvm"
                         repo="llvm-project"
-                        stack={vec!["cplusplus".to_owned(), "c".to_owned()]}
+                        stack={vec!["cplusplus".to_owned(), "c".to_owned(), "cmake".to_owned()]}
                         contribution="My work on LLVM has mostly been within LLVM's C interface, a way to access LLVM APIs from the C programming language. Here I've worked on mapping functions and adding tests related to the LLVM IR and LLVM OrcJITv2 runtime compiler."
                         description="LLVM is a monolithic compiler infrastructure project hosting a number of C and C++ compilers, optimizers, and standard library implementations. It's also the compiler backend for a significant amount of programming languages such as C, C++, Rust, and Swift"
                     />
@@ -82,7 +82,7 @@ fn home_projects_section() -> Html {
                         repo="monoweb"
                         stack={vec!["typescript".to_owned(), "react".to_owned(), "nextjs".to_owned(), "aws".to_owned(), "terraform".to_owned()]}
                         contribution="During my time as an Online and Dotkom member I've contributed to numerous parts of our system including our design system, our cloud infrastructure hosted on Amazon Web Services, and most recently; our new events system."
-                        description="Monoweb is the new iteration of Online's (my study programme's student association) website. It's a large system with many components varying in complexity. Monoweb is implemented as full-stack TypeScript and is the successor to our legacy Python system."
+                        description="Monoweb is the new iteration of my study programme's student association's website. It's a large system with many components varying in complexity. Monoweb is implemented as full-stack TypeScript and is the successor to our legacy Python system."
                     />
                 </div>
             </div>
@@ -111,7 +111,7 @@ fn home_experience_section() -> Html {
             Date::new_with_year_month(2021, 7),
             "Frontend Developer (part-time)".to_owned(),
             vec![
-                "Lead planning, development, and design for static web content platform used to promote electrical engineering to norwegian high school students.".to_owned(),
+                "Led planning, development, and design for static web content platform used to promote electrical engineering to norwegian high school students.".to_owned(),
                 "Was responsible for deployment to production environment to static file host over FTP with nightly builds deploying to Vercel.".to_owned()
             ],
             vec!["typescript".to_owned(), "react".to_owned(), "nextjs".to_owned()],
@@ -122,7 +122,7 @@ fn home_experience_section() -> Html {
         <Section class="bg-gray-2" id="experience">
             <div class="p-3">
                 <Heading>{"💼Experience"}</Heading>
-                <Paragraph>{"Throughout the years I've been lucky enough to work for a few companies where I've grown a lot as a developer as well has having the opportunity to work in teams on larger scale software. This is a timeline highlighting my work at these companies and what roles I've taken."}</Paragraph>
+                <Paragraph>{"Throughout the years I've been lucky enough to work for a few companies where I've grown a lot as a developer, as well has having the opportunity to work in teams on larger scale software. This is a timeline highlighting my work at these companies and what roles I've taken."}</Paragraph>
 
                 <div class="my-4">
                     <Timeline events={events} />
